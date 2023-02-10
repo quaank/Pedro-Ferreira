ab5ce29aa5e2447ab9e2aeb202e3e3a4f5437cb4
NunesMig <miguelnunesnova@gmail.com>
2023-02-10 16:54:02 +0000

    12

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        </p>
+     <h1>Olá Vascão</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
